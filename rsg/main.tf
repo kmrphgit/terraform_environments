cbd30b9f8fa8f17ba26e87f9556038a909de3f56
kmrphgit <will.barbier@kloudmorph.com>
2020-05-12 16:05:53 -0400

    updated rsg name variable

--- rsg/main.tf
+++ rsg/main.tf
@@ -7,7 +7,7 @@ terraform {
 
 resource "azurerm_resource_group" "rsg" {
     # name     = "${var.client_code}-${upper(var.az_region_code)}-${var.tags.tag-Environment}-${var.role_code}-RSG"
-    name     = "${locals.name_prefix}-RSG"
+    name     = "${local.name_prefix}-RSG"
     location = var.az_region
 
     tags     = var.tags
