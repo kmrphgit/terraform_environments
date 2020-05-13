d7a01ac69fbe4ddff416111bbf31cc01dba0bca0
kmrphgit <will.barbier@kloudmorph.com>
2020-05-13 11:59:15 -0400

    updated rsg variables

--- rsg/variables.tf
+++ rsg/variables.tf
@@ -6,6 +6,10 @@ variable "role_code" {
   type        = string
   description = "Short-hand code for descriptive name of role for resource group e.g. API, UI, NET, SQL, etc"
 }
+variable "az_region" {
+  type        = string
+  description = "Azure resource region location"
+}
 # variable "tf_state_sa_rsg" {
 #   type        = string
 #   description = "Resource group name for Terraform State storage account"
