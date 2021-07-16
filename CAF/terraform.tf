23b2944acb66c7df2918b10ee33620439a256ff0
testusermsft103@outlook.com <testusermsft103@outlook.com>
2021-07-15 23:53:38 -0400

    changes

--- CAF/terraform.tf
+++ CAF/terraform.tf
@@ -15,10 +15,16 @@ provider "azurerm" {
       delete_nested_items_during_deletion = false
     }
   }
-  subscription_id = module.globals.spn.subs.subscription_id
-  tenant_id       = module.globals.spn.subs.tenant_id
-  client_id       = module.globals.spn.subs.client_id
-  client_secret   = module.globals.spn.subs.client_secret
+  # subscription_id = module.globals.spn.subs.subscription_id
+  # tenant_id       = module.globals.spn.subs.tenant_id
+  # client_id       = module.globals.spn.subs.client_id
+  # client_secret   = module.globals.spn.subs.client_secret
+
+  subscription_id = var.spn.subs.subscription_id
+  tenant_id       = var.spn.subs.tenant_id
+  client_id       = var.spn.subs.client_id
+  client_secret   = var.spn.subs.client_secret
+
 }
 
 data "azurerm_subscription" "primary" {}
