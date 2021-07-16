12330a2dba5e7afe719de877c476779a9f296a65
testusermsft103@outlook.com <testusermsft103@outlook.com>
2021-07-16 05:29:35 -0400

    changes

--- management_group/main.tf
+++ management_group/main.tf
@@ -1,18 +1,21 @@
-resource "azurerm_management_group" "platform" {
-  display_name = var.settings.platform.name
-}
-
-resource "azurerm_management_group" "prod" {
-  display_name = var.settings.prod.name
-  parent_management_group_id = azurerm_management_group.platform.id
-}
-
-resource "azurerm_management_group" "nonprod" {
-  display_name = var.settings.nonprod.name
-  parent_management_group_id = azurerm_management_group.platform.id
-}
-
-# resource "azurerm_management_group" "root" {
-#   display_name = var.settings.root.name
+# resource "azurerm_management_group" "platform" {
+#   display_name = var.settings.platform.name
 # }
 
+# resource "azurerm_management_group" "prod" {
+#   display_name = var.settings.prod.name
+#   parent_management_group_id = azurerm_management_group.platform.id
+# }
+
+# resource "azurerm_management_group" "nonprod" {
+#   display_name = var.settings.nonprod.name
+#   parent_management_group_id = azurerm_management_group.platform.id
+# }
+
+# # resource "azurerm_management_group" "root" {
+# #   display_name = var.settings.root.name
+# # }
+
+locals {
+    settings = var.settings
+}
