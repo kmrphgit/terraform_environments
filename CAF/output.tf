df332fe8f945040c2d7ae26d16022d8d7851728a
testusermsft103@outlook.com <testusermsft103@outlook.com>
2021-07-16 01:06:00 -0400

    changes

--- CAF/output.tf
+++ CAF/output.tf
@@ -6,7 +6,7 @@
 # }
 
 output "naming_conventions" {
-  value = module.globals.eastus2.naming_conventions #.naming_conventions.region1.naming_conventions
+  value = module.globals.global.eastus2.naming_conventions #.naming_conventions.region1.naming_conventions
 }
 
 output "spn" {
