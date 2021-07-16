c376dd85cb94f55016742103832146de5f0ba593
testusermsft103@outlook.com <testusermsft103@outlook.com>
2021-07-16 10:04:20 -0400

    changes

--- CAF/identity/networking.tf
+++ CAF/identity/networking.tf
@@ -2,6 +2,7 @@ module "networking" {
   source   = "git::https://github.com/kmrphgit/terraform_modules.git//networking/vnet"
   for_each = var.settings.vnet
   rg_name = module.rg.rg_name
+  settings = var.settings
   # location = var.settings.location
   # rg_name  = "${var.settings.naming_conventions.resource_group}-${var.settings["eastus2"].resource_group[each.key].iteration}"
 }
