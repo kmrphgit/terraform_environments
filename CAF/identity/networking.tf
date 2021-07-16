56518da2566fd648edd2b81a0990d6d46457f6e7
testusermsft103@outlook.com <testusermsft103@outlook.com>
2021-07-16 10:27:53 -0400

    changes

--- CAF/identity/networking.tf
+++ CAF/identity/networking.tf
@@ -3,7 +3,7 @@ module "networking" {
   for_each = var.settings.vnet
 
   iteration = each.key
-  rg_name = module.rg.rg_name
+  rg_name = module.rg["001"].rg_name
   settings = var.settings
   # location = var.settings.location
   # rg_name  = "${var.settings.naming_conventions.resource_group}-${var.settings["eastus2"].resource_group[each.key].iteration}"
