bb96a2f8efbe0e025f42e29c636a37dbbc5851b6
testusermsft103@outlook.com <testusermsft103@outlook.com>
2021-07-16 09:23:38 -0400

    changes

--- CAF/identity/networking.tf
+++ CAF/identity/networking.tf
@@ -3,5 +3,4 @@ module "networking" {
   for_each = var.settings["eastus2"].resource_group
 
   rg_name  = "${var.settings.naming_conventions.resource_group}-${var.settings["eastus2"].resource_group[each.key].iteration}"
-  location = var.settings.location
 }
