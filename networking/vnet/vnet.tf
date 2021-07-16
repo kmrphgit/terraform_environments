d6d3558e1803076edcec7aa37f0b299344ce3f79
testusermsft103@outlook.com <testusermsft103@outlook.com>
2021-07-16 09:30:26 -0400

    changes

--- networking/vnet/vnet.tf
+++ networking/vnet/vnet.tf
@@ -1,7 +1,7 @@
 resource "azurerm_virtual_network" "vnet" {
   name                = "${var.settings.naming_conventions.vnet}-${var.settings.iteration}"
   location            = var.settings.location
-  resource_group_name = var.resource_group_name
+  resource_group_name = var.rg_name
   address_space       = var.settings.vnet.address_space
 #   tags                = local.tags
 
