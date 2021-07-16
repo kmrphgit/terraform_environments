26b13138a2b75239748e6901a250ea2c99a92522
testusermsft103@outlook.com <testusermsft103@outlook.com>
2021-07-16 09:18:41 -0400

    changes

---
+++ networking/vnet/vnet.tf
@@ -0,0 +1,18 @@
+resource "azurerm_virtual_network" "vnet" {
+  name                = "${var.settings.naming_conventions.vnet}-${var.settings.iteration}"
+  location            = var.settings.location
+  resource_group_name = var.resource_group_name
+  address_space       = var.settings.vnet.address_space
+#   tags                = local.tags
+
+#   dns_servers = lookup(var.settings.vnet, "dns_servers", null)
+
+#   dynamic "ddos_protection_plan" {
+#     for_each = var.ddos_id != "" ? [1] : []
+
+#     content {
+#       id     = var.ddos_id
+#       enable = true
+#     }
+#   }
+}
