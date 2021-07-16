39c3a2094fb580a55c27a9260af3fccde75d34ff
testusermsft103@outlook.com <testusermsft103@outlook.com>
2021-07-16 05:11:34 -0400

    changes

---
+++ management_group/main.tf
@@ -0,0 +1,18 @@
+resource "azurerm_management_group" "platform" {
+  display_name = var.settings.root.name
+}
+
+resource "azurerm_management_group" "prod" {
+  display_name = var.settings.prod.name
+  parent_management_group_id = azurerm_management_group.platform.id
+}
+
+resource "azurerm_management_group" "nonprod" {
+  display_name = var.settings.nonprod.name
+  parent_management_group_id = azurerm_management_group.platform.id
+}
+
+# resource "azurerm_management_group" "root" {
+#   display_name = var.settings.root.name
+# }
+
