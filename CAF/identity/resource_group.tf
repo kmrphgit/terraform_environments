255f3635e542a6003b405a6385c8be8ad68b6d0c
testusermsft103@outlook.com <testusermsft103@outlook.com>
2021-07-16 10:03:03 -0400

    changes

--- CAF/identity/resource_group.tf
+++ CAF/identity/resource_group.tf
@@ -2,6 +2,5 @@ module "rg" {
   source   = "git::https://github.com/kmrphgit/terraform_modules.git//resource_group"
   for_each = var.settings.resource_group
 
-  rg_name  = "${var.settings.naming_conventions.resource_group}-${var.settings.resource_group[each.key].iteration}"
-  location = var.settings.location
+  settings = var.settings
 }
