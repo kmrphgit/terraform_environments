56ba253cd4b6ed65ae62d6c6ff1d9a5d34c09927
testusermsft103@outlook.com <testusermsft103@outlook.com>
2021-07-15 22:53:11 -0400

    changes

--- CAF/variables.tf
+++ CAF/variables.tf
@@ -12,63 +12,67 @@ variable "locations" {
   default = []
 }
 
-
-variable "compute" {
-  description = "Configuration object - Azure compute resources"
-  default = {
-    virtual_machines = {}
-  }
+variable "settings" {
+  default = {}
 }
 
-variable "database" {
-  description = "Configuration object - databases resources"
-  default     = {}
-}
 
-## Networking variables
-variable "networking" {
-  description = "Configuration object - networking resources"
-  default     = {}
-}
+# variable "compute" {
+#   description = "Configuration object - Azure compute resources"
+#   default = {
+#     virtual_machines = {}
+#   }
+# }
 
-## Security variables
-variable "security" {
-  description = "Configuration object - security resources"
-  default     = {}
-}
+# variable "database" {
+#   description = "Configuration object - databases resources"
+#   default     = {}
+# }
 
-variable "keyvaults" {
-  description = "Configuration object - Azure Key Vault resources"
-  default     = {}
-}
+# ## Networking variables
+# variable "networking" {
+#   description = "Configuration object - networking resources"
+#   default     = {}
+# }
 
-variable "keyvault_access_policies" {
-  description = "Configuration object - Azure Key Vault policies"
-  default     = {}
-}
+# ## Security variables
+# variable "security" {
+#   description = "Configuration object - security resources"
+#   default     = {}
+# }
 
-variable "storage_accounts" {
-  description = "Configuration object - Storage account resources"
-  default     = {}
-}
+# variable "keyvaults" {
+#   description = "Configuration object - Azure Key Vault resources"
+#   default     = {}
+# }
 
-variable "log_analytics" {
-  description = "Configuration object - Log Analytics resources."
-  default     = {}
-}
+# variable "keyvault_access_policies" {
+#   description = "Configuration object - Azure Key Vault policies"
+#   default     = {}
+# }
 
-variable "subscriptions" {
-  description = "Configuration object - Subscriptions."
-  default     = {}
-}
+# variable "storage_accounts" {
+#   description = "Configuration object - Storage account resources"
+#   default     = {}
+# }
 
-variable "identity_settings" {
-  description = "Configuration object - Identity."
-  default     = {}
-}
+# variable "log_analytics" {
+#   description = "Configuration object - Log Analytics resources."
+#   default     = {}
+# }
 
-variable "management_settings" {
-  description = "Configuration object - Management."
-  default     = {}
-}
+# variable "subscriptions" {
+#   description = "Configuration object - Subscriptions."
+#   default     = {}
+# }
+
+# variable "identity_settings" {
+#   description = "Configuration object - Identity."
+#   default     = {}
+# }
+
+# variable "management_settings" {
+#   description = "Configuration object - Management."
+#   default     = {}
+# }
 
