477f725bcfa2bef8546386e2645fc0f248214e33
testusermsft103@outlook.com <testusermsft103@outlook.com>
2021-07-28 09:39:46 -0400

    changes

--- CAF/identity/networking.tf
+++ CAF/identity/networking.tf
@@ -2,7 +2,7 @@ module "networking" {
   source   = "git::https://github.com/kmrphgit/terraform_modules.git//networking/vnet"
   for_each = var.settings.networking.vnets
 
-  rg_name   = module.rg.rg[each.value.rg_key].name
+  rg_name   = module.rg.[each.value.rg_key].rg_name
   iteration = each.key
   settings  = merge(module.globals.settings, each.value)
   location  = var.settings.location
