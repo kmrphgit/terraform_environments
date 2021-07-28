958c607df96a8580d27a84dacdcf2ea22dc02465
testusermsft103@outlook.com <testusermsft103@outlook.com>
2021-07-28 06:09:26 -0400

    changes

--- CAF/identity/resource_group.tf
+++ CAF/identity/resource_group.tf
@@ -4,6 +4,6 @@ module "rg" {
   for_each   = var.settings.resource_group
 
   iteration = each.key
-  location  = var.location
+  location  = var.settings.location
   settings  = merge(module.globals.settings, each.value)
 }
