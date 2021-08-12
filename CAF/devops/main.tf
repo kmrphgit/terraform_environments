9afb1a4b9f1864bef8ae25b91c5bd02408875f81
testusermsft103@outlook.com <testusermsft103@outlook.com>
2021-08-12 00:53:42 -0400

    changes

--- CAF/devops/main.tf
+++ CAF/devops/main.tf
@@ -1,7 +1,5 @@
 module "globals" {
   source = "git::https://github.com/kmrphgit/terraform_global_configs.git//global"
-  # applicationName = var.WORKSPACE
-  # environment     = var.ENVIRONMENT
   applicationName = var.settings.applicationName
   environment     = var.settings.environment
   location        = var.settings.location
