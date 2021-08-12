2fd986917bfdf49096e60e4711904b1e00609752
testusermsft103@outlook.com <testusermsft103@outlook.com>
2021-08-12 03:03:57 -0400

    changes

--- CAF/output.tf
+++ CAF/output.tf
@@ -1,15 +1,15 @@
-output "devops_prod" {
-  value = module.devops_prod
-}
+#output "devops_prod" {
+#  value = module.devops_prod
+#}
 
-output "governance" {
-  value = module.governance
-}
+#output "governance" {
+#  value = module.governance
+#}
 
-output "identity_nonprod" {
-  value = module.identity_nonprod
-}
+#output "identity_nonprod" {
+#  value = module.identity_nonprod
+#}
 
-output "identity_prod" {
-  value = module.identity_prod
-}
+#output "identity_prod" {
+#  value = module.identity_prod
+#}
